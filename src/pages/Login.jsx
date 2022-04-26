9bbe907d89e3ee31b4f913723dc425fb1c46f278
Sidali <gh.fullstack@gmail.com>
2022-04-26 12:21:37 +0100

    form added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../features/auth/authSlice'
 
 import '../style/loginPage.css'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const {email, password} = formData
+
+  const dispatch = useDispatch()
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const userData = {email, password}
+    dispatch(login(userData))
+  }
   return (
-   <section className='login-section'>
-     <h1>Login</h1>
-   </section>
+    <section className='login-section'>
+      <div className='login-main_container'>
+        <div className='login-top_container'>
+          <h1>Login to your account</h1>
+        </div>
+        <div className='login-bottom_container'>
+          <form onSubmit={onSubmit} className='form-container'>
+            <input
+              type='email'
+              className='form-input'
+              name='email'
+              value={email}
+              placeholder='Email'
+              onChange={onChange}
+            />
+            <input
+              type='password'
+              className='form-input'
+              name='password'
+              value={password}
+              placeholder='Password'
+              onChange={onChange}
+            />
+            <button className='btn-form'>Login</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
+
 import '../style/registerPage.css'
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '', 
+    password: ''
+  })
+
+  const {name, email, password} = formData
+
+  const dispatch = useDispatch()
+
+  const onChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const userData = {name, email, password}
+    dispatch(register(userData))
+  }
   return (
     <section className='register-section'>
-      <h1>Create a new account</h1>
+      <div className='register-main_container'>
+        <div className='register-top_container'>
+          <h1>Create a new account</h1>
+        </div>
+        <div className='register-bottom_container'>
+          <form onSubmit={onSubmit} className='form-container'>
+            <input
+              type='text'
+              className='form-input'
+              name='name'
+              value={name}
+              placeholder='Name'
+              onChange={onChange}
+            />
+            <input
+              type='email'
+              className='form-input'
+              name='email'
+              value={email}
+              placeholder='Email'
+              onChange={onChange}
+            />
+            <input
+              type='password'
+              className='form-input'
+              name='password'
+              value={password}
+              placeholder='Password'
+              onChange={onChange}
+            />
+            <button className='btn-form'>Register</button>
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
